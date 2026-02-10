a23976d4cbe002378a89324ac3749b3a5b88d82b
Painelai <painelweb.a.i@gmail.com>
2026-02-10 20:13:51 -0300

    Add missing import statement in LoadingScreen

--- components/LoadingScreen.tsx
+++ components/LoadingScreen.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect, useRef, useMemo } from 'react';
 import { ShieldCheck, Globe2 } from 'lucide-react'; 
 import { UI_TRANSLATIONS, getStatusMessages } from '../core/translations';
