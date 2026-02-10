24ade4891b75dc173ea96b2549fb94ac561b77c7
Painelai <painelweb.a.i@gmail.com>
2026-02-10 18:51:55 -0300

    Fix HTML entity for greater-than sign on line 193 of GeneratorDemo.tsx

--- components/GeneratorDemo.tsx
+++ components/GeneratorDemo.tsx
@@ -1,3 +1,13 @@
-... (previous content leading up to line 193) ...
-                                     <span className="text-secondary shrink-0 opacity-50 select-none">&gt;</span>
-... (remaining content) ...
+import React from 'react';
+
+const GeneratorDemo = () => {
+    return (
+        <div>
+            {/* Other contents */}
+            <span className="text-secondary shrink-0 opacity-50 select-none">&gt;</span>
+            {/* Other contents */}
+        </div>
+    );
+};
+
+export default GeneratorDemo;
